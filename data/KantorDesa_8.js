d252d53fae14a7932590d276f4ea4892d53080b2
ajihan426 <57880806+ajihan426@users.noreply.github.com>
2019-12-16 02:02:52 +0700

    Add files via upload

---
+++ data/KantorDesa_8.js
@@ -0,0 +1,16 @@
+var json_KantorDesa_8 = {
+"type": "FeatureCollection",
+"name": "KantorDesa_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Barukan Mns", "ELEVATION": 308.226, "ND": "Barukan", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.50609, -7.699512 ] } },
+{ "type": "Feature", "properties": { "NAME": "Bendan Mns", "ELEVATION": 290.202, "ND": "Bendan", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.493644, -7.691674 ] } },
+{ "type": "Feature", "properties": { "NAME": "Borangan Mns", "ELEVATION": 271.456, "ND": "Borangan", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.514148, -7.695126 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kebonalas Mns", "ELEVATION": 279.387, "ND": "Kebonalas", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.489497, -7.688189 ] } },
+{ "type": "Feature", "properties": { "NAME": "Leses Mns", "ELEVATION": 291.884, "ND": "Leses", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.483125, -7.693561 ] } },
+{ "type": "Feature", "properties": { "NAME": "Nangsri Mns", "ELEVATION": 239.012, "ND": "Nangsri", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.508878, -7.706904 ] } },
+{ "type": "Feature", "properties": { "NAME": "Solodiran Mns", "ELEVATION": 290.682, "ND": "Solodiran", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.490709, -7.70545 ] } },
+{ "type": "Feature", "properties": { "NAME": "TanjungsariMns", "ELEVATION": 293.085, "ND": "Tanjungsari", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.494055, -7.696528 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tijayan Mns", "ELEVATION": 315.196, "ND": "Tijayan", "NK": "Manisrenggo", "NKB": "Klaten" }, "geometry": { "type": "Point", "coordinates": [ 110.499279, -7.687761 ] } }
+]
+}
---
+++ data/KantorKecamatan_9.js
@@ -0,0 +1,8 @@
+var json_KantorKecamatan_9 = {
+"type": "FeatureCollection",
+"name": "KantorKecamatan_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Manisrenggo", "ELEVATION": 289.961 }, "geometry": { "type": "Point", "coordinates": [ 110.491853, -7.694893 ] } }
+]
+}
